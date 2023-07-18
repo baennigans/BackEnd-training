da4d1d6077cd44a6e85a8d1b6a6cec88b03e15f8
DESKTOP-JCII05P\User <bgh2584@naver.com>
2023-07-18 17:39:11 +0900

    Spring di

---
+++ src/main/java/di/java/DIMain.java
@@ -0,0 +1,23 @@
+package di.java;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DIMain {
+
+	public static void main(String[] args) {
+		
+//		ApplicationContext context = new GenericXmlApplicationContext("di-anno.xml");
+		ApplicationContext context = new GenericXmlApplicationContext("di-anno2.xml");
+		
+		Car car = context.getBean("car", Car.class);
+		car.printTireBrand();
+		
+		/*
+		 	Tire tire = new HankookTire();
+		 	Car c = new Car();
+		 	c.setTire(tire)
+		 */
+	}
+}
+
