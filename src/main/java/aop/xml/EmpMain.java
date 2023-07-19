3fd0b8d28cd65c84f6a7926143652d023bb412f9
DESKTOP-JCII05P\User <bgh2584@naver.com>
2023-07-19 13:36:17 +0900

    Spring aop

---
+++ src/main/java/aop/xml/EmpMain.java
@@ -0,0 +1,23 @@
+package aop.xml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+/*
+ * SPRING AOP 특징
+ * 1. Runtime 기반
+ * 2. Proxy 기반
+ * 3. interface 기반
+ */
+
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new GenericXmlApplicationContext("aop-xml.xml");
+	
+		Programmer programmer = context.getBean("programmer",Programmer.class);
+		programmer.work();
+	}
+}
