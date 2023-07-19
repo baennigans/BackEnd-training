730e34475ee79a99c9a186cbfdc9091332b652c3
DESKTOP-JCII05P\User <bgh2584@naver.com>
2023-07-19 15:13:45 +0900

    aop annotation, config

---
+++ src/main/java/aop/anno/Designer.java
@@ -0,0 +1,9 @@
+package aop.anno;
+
+public class Designer implements Employee {
+	
+	@Override
+	public void work() {
+		System.out.println("디자인 시안 및 수정합니다.");
+	}
+}
---
+++ src/main/java/aop/java/Designer.java
@@ -0,0 +1,12 @@
+package aop.java;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Designer implements Employee {
+	
+	@Override
+	public void work() {
+		System.out.println("디자인 시안 및 수정합니다.");
+	}
+}
