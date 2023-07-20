49192c884c16647380b400aa7d3ddb81a20944f9
DESKTOP-JCII05P\User <bgh2584@naver.com>
2023-07-20 14:11:28 +0900

    Mybatis DAO/xml

--- src/board/dao/BoardDAO.java
+++ src/board/dao/BoardDAO.java
@@ -1,10 +1,18 @@
 package board.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.ibatis.session.SqlSession;
+
+import board.vo.BoardVO;
 
 public class BoardDAO {
 
 	private SqlSession session;
+	
 	
 	public BoardDAO() {
 		session = new MyConfig().getInstance();
@@ -12,16 +20,102 @@ public class BoardDAO {
 	
 	
 	public void work() {
-		insert();
+//		insert();
+//		select();
+//		selectOne();
+//		selectWhere();
+//		selectWhere2();
+		selectWhere3();
 	}
 	
 	
 	public void insert() {
 		
-		String title = "이번에는 꼭!!";
+		BoardVO board = new BoardVO();
+		board.setTitle("VO로 삽입");
+		board.setWriter("hong");
 		
-		int cnt = session.insert("board.dao.BoardDAO.insert", title);
+		int cnt = session.insert("board.dao.BoardDAO.insert", board);
 		session.commit();
 		System.out.println("총 삽입 개수 : "+cnt);
 	}
+	
+	
+	public void select() {
+		//전체게시글 조회
+//		List<BoardVO> list = session.selectList("board.dao.BoardDAO.selectAll");
+		List<BoardVO> list = session.selectList("board.dao.BoardDAO.selectAll2");
+		
+		for(BoardVO board : list) {
+			System.out.println(board);
+		}
+	}
+	
+	
+	public void selectOne() {
+		
+		int boardNo = 41;
+		BoardVO result = session.selectOne("board.dao.BoardDAO.selectByNo", boardNo);
+		System.out.println(result);
+		
+		BoardVO board = new BoardVO();
+		board.setNo(41);
+		result = session.selectOne("board.dao.BoardDAO.selectByNo2", board);
+		System.out.println(result);
+	}
+	
+	
+	public void selectWhere() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("제목이오");
+//		board.setWriter("홍길둔");
+		
+		List<BoardVO> list = session.selectList("board.dao.BoardDAO.selectWhere", board);
+		for(BoardVO b : list) {
+			System.out.println(b);
+		}
+	}
+	
+	
+	public void selectWhere2() {
+		
+		Map<String, String> board = new HashMap<>();
+		board.put("title", "제목이오");
+		board.put("writer", "홍길동");
+		
+		List<BoardVO> list = session.selectList("board.dao.BoardDAO.selectWhere2", board);
+		for(BoardVO b : list) {
+			System.out.println(b);
+		}
+	}
+	
+	
+	public void selectWhere3() {
+		
+		Map<String, String> board = new HashMap<>();
+		board.put("no", "41");
+		
+		Map<String, Object> result = session.selectOne("board.dao.BoardDAO.selectWhere3", board);
+		
+		Set<String> keys = result.keySet();
+		for(String key : keys) {
+			System.out.println("key : "+key+", value : "+result.get(key));
+		}
+		
+	}
+	
+	
+	public void selectNos() {
+		int[] nos = {1, 2, 4, 5, 7, 10, 20, 21, 22};
+		
+		List<BoardVO> list = session.selectList("board.dao.BoardDAO.selectNos", nos);
+		
+		for(BoardVO b : list) {
+			System.out.println(b);
+		}
+	}
+	
+	
+	
 }
