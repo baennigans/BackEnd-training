6951a1d74f47a579a889ac24280d922eb3c8e35a
DESKTOP-JCII05P\User <bgh2584@naver.com>
2023-07-21 09:56:39 +0900

    ResponseBody 수정

--- src/main/java/kr/co/mlec/body/ResBodyController.java
+++ src/main/java/kr/co/mlec/body/ResBodyController.java
@@ -5,64 +5,71 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.mlec.form.MemberVO;
 
-@Controller
+
 @RequestMapping("/ajax")
+@RestController
 public class ResBodyController {
 
-	@RequestMapping("/resBody.do")
-	@ResponseBody
-	public String resStringBody() {
-		return "OK, 성공";
-	}
-
-	@RequestMapping("/resBody.json")
-	@ResponseBody
-	public Map<String, String> resJsonBody() {
-		Map<String, String> result = new HashMap<>();
-		result.put("id", "sbc");
-		result.put("name", "hong");
-		result.put("addr", "서울");
-		return result;
-	}
-
-	@RequestMapping("/resVOBody.json")
-	@ResponseBody
-	public MemberVO resJsonVOBody() {
-		MemberVO vo = new MemberVO();
-		vo.setId("sbc");
-		vo.setName("sbc");
-		vo.setPassword("1234");
-		return vo;
-	}
-
-	@RequestMapping("/resStringListBody.json")
-	@ResponseBody
-	public List<String> resJsonStringListBody() {
-		List<String> list = new ArrayList<>();
-		for (int i = 1; i < 4; i++) {
-			list.add(String.valueOf(i));
-		}
-		return list;
-	}
-
+//	@ResponseBody
 	@RequestMapping("/resVOListBody.json")
-	@ResponseBody
 	public List<MemberVO> resJsonVOListBody() {
 		List<MemberVO> list = new ArrayList<>();
-		for (int i = 1; i < 4; i++) {
+
+		for (int i = 1; i <= 4; i++) {
 			MemberVO vo = new MemberVO();
-			vo.setId("sbc");
-			vo.setName("sbc");
+			vo.setId("hong");
 			vo.setPassword("1234");
+			vo.setName("홍길동");
 			list.add(vo);
 		}
 		return list;
 	}
 
+	
+//	@ResponseBody
+	@RequestMapping("/resStringListBody.json")
+	public List<String> resJsonStringListBody() {
+		List<String> list = new ArrayList<String>();
+
+		for (int i = 1; i <= 4; i++) {
+			list.add(String.valueOf(i));
+		}
+		return list;
+	}
+
+	
+//	@ResponseBody
+	@RequestMapping("/resVOBody.json")
+	public MemberVO resJsonVOBody() {
+		MemberVO vo = new MemberVO();
+		vo.setId("hong");
+		vo.setName("홍길동");
+		vo.setPassword("1234");
+		return vo;
+	}
+
+	
+//	@ResponseBody
+	@RequestMapping("/resBody.json")
+	public Map<String, String> resJsonBody() {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("id", "hong");
+		result.put("name", "홍길동");
+		result.put("addr", "서울");
+		return result;
+	}
+
+	
+//	@ResponseBody
+	@RequestMapping("/resBody.do")
+	public String resStringBody() {
+		return "OK, 성공";
+	}
+	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,13 +7,14 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<a href="<%=request.getContextPath()%>/hello/hello.do"> hello </a> <br>
-	<a href="<%=request.getContextPath()%>/method/method.do"> method </a> <br>
-	<a href="<%=request.getContextPath()%>/form/joinForm.do"> form </a> <br>
-	<a href="<%=request.getContextPath() %>/ajax/resBody.do">문자열 응답</a> <br>
-	<a href="<%=request.getContextPath() %>/ajax/resBody.json">JSON 응답</a> <br>
-	<a href="<%=request.getContextPath() %>/ajax/resVOBody.json">JSON VO 응답</a> <br>
-	<a href=“…………/ajax/resStringListBody.json">JSON List(문자열) 응답</a> <br>
-	<a href=“…………/ajax/resVOListBody.json">JSON List(VO) 응답</a>
+	<a href="<%= request.getContextPath() %>/hello/hello.do">hello</a> <br>
+	<a href="<%= request.getContextPath() %>/method/method.do">method</a> <br>
+	<a href="<%= request.getContextPath() %>/form/joinForm.do">form</a> <br>
+	<a href="<%= request.getContextPath() %>/ajax/resBody.do">문자열 응답</a> <br>
+	<a href="<%= request.getContextPath() %>/ajax/resBody.json">JSON 응답</a> <br>
+	<a href="<%= request.getContextPath() %>/ajax/resVOBody.json">JSON VO 응답</a> <br>
+	<a href="<%= request.getContextPath() %>/ajax/resStringListBody.json">JSON List(문자열) 응답</a> <br>
+	<a href="<%= request.getContextPath() %>/ajax/resVOListBody.json">JSON List(vo) 응답</a> <br>
+	<a href="<%= request.getContextPath() %>/file/uploadForm.do">파일업로드</a>
 </body>
 </html>
